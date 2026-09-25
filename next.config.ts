c48c092257c6332f27a1afec0d80e391d9679127
ArturBaghdanyan <arturbaghdanyan20@gmail.com>
2026-09-25 14:22:22 +0400

    Configure Next.js API rewrites for EC2 proxy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://35.171.55.191/api/:path*",
+      },
+    ];
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
